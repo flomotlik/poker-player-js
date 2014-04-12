2fc0927e529c0cae3b23eb97ec9eda3d25219e4e
Florian Motlik <flo@codeship.io>
2014-04-12 12:06:00 +0200

    some commit

--- player.js
+++ player.js
@@ -2,13 +2,13 @@
 module.exports = {
 
   VERSION: "Default JavaScript folding player",
+  RANKS: [2,3,4,5,6,7,8,9,10,"J","Q","K","A"],
 
   bet_request: function(game_state) {
     if (typeof(game_state) !== 'object') game_state = JSON.parse(game_state);
 
     players = game_state.players
     our_player = game_state.players[game_state.in_action]
-    console.log("our player: " + our_player);
 
     current_buy_in = game_state.current_buy_in
     console.log("buy in: " + current_buy_in);
@@ -20,14 +20,26 @@ module.exports = {
     console.log("bet: " + bet);
 
     hole_cards = our_player.hole_cards;
-    minimum_raise = game_state.minimum_raise
-    console.log("minimum raise: " + minimum_raise);
+    minimum_raise = game_state.minimum_raise;
+    console.log("minimumraise: " + minimum_raise);
 
-    raise = current_buy_in - bet + minimum_raise;
+    call = current_buy_in - bet;
+    raise = call + minimum_raise;
+    console.log("Call: " + call);
+    console.log("Raise: " + raise);
 
-    console.log(raise);
+    first_card = hole_cards[0];
+    second_card = hole_cards[1];
 
-    return raise;
+    if(first_card.rank == second_card.rank) {
+      return raise * 2;
+    }
+
+    if (this.RANKS.indexOf(first_card.rank) > 7 || this.RANKS.indexOf(second_card.rank) > 7){
+      return call;
+    }
+
+    return 0;
   },
 
   showdown: function(game_state) {
--- spec/test_spec.js
+++ spec/test_spec.js
@@ -2,8 +2,33 @@ mockObj = require('./mock.js');
 player = require('../player.js')
 
 describe("Apoker suite", function() {
-  it("test", function() {
-    console.log(mockObj.toString());
-    expect(player.bet_request(mockObj)).toEqual(480)
+  it("should raise by twice of the minimum", function() {
+    mockObj.players[mockObj.in_action].hole_cards = [                         // The cards of the player. This is only visible for your own player
+                                            //     except after showdown, when cards revealed are also included.
+        {
+            "rank": "6",                    // Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+            "suit": "hearts"                // Suit of the card. Possible values are: clubs,spades,hearts,diamonds
+        },
+        {
+            "rank": "6",
+            "suit": "spades"
+        }
+    ];
+    expect(player.bet_request(mockObj)).toEqual(960)
+  });
+
+  it("should call", function() {
+    mockObj.players[mockObj.in_action].hole_cards = [                         // The cards of the player. This is only visible for your own player
+                                            //     except after showdown, when cards revealed are also included.
+        {
+            "rank": "K",                    // Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+            "suit": "hearts"                // Suit of the card. Possible values are: clubs,spades,hearts,diamonds
+        },
+        {
+            "rank": "6",
+            "suit": "spades"
+        }
+    ];
+    expect(player.bet_request(mockObj)).toEqual(240)
   });
 });
