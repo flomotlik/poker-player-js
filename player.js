ae5fdf4ccdcf09238a0147ee8bd3f77ff3f0e299
Florian Motlik <flo@codeship.io>
2014-04-12 11:04:43 +0200

    some commit

--- player.js
+++ player.js
@@ -8,15 +8,22 @@ module.exports = {
 
     players = game_state.players
     our_player = game_state.players[game_state.in_action]
+    console.log("our player: " + our_player)
 
     current_buy_in = game_state.current_buy_in
+    console.log("buy in: " + current_buy_in)
     in_action = game_state.in_action
+    console.log("in action: " + in_action)
 
     bet = our_player.bet
 
+    console.log("bet: " + bet)
+
     hole_cards = our_player.hole_cards;
     minimum_raise = game_state.minimum_raise
-    raise = game_state.current_buy_in - players[in_action][bet] + minimum_raise;
+    console.log("minimum raise: " + minimum_raise)
+
+    raise = current_buy_in - players[in_action][bet] + minimum_raise;
 
     console.log(raise);
 
